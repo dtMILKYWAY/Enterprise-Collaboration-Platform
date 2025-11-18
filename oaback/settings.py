8455041ed6270a17221b42de029c5262f4366c37
滑稽✨先生 <105369060+dtMILKYWAY@users.noreply.github.com>
2025-11-18 12:53:28 +0800

    Update settings.py

--- oaback/settings.py
+++ oaback/settings.py
@@ -57,8 +57,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # close CSRf
-    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -155,5 +153,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 #覆盖djingo自带的User模型
 AUTH_USER_MODEL = 'oaauth.OAUser'
+
 
 
