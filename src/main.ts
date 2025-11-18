6ce4c9d3408f6bc9a80b5435b6e63071d07b093a
滑稽✨先生 <105369060+dtMILKYWAY@users.noreply.github.com>
2025-11-18 12:23:08 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-const pinia = createPinia() // 创建 Pinia 实例
+const pinia = createPinia()
 
 app.use(router)
-app.use(pinia) // 使用 Pinia
+app.use(pinia) 
 
 app.mount('#app')
